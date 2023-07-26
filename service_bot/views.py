4cde874e52fc4c8cd89ddbb20192b5fe1dd6c8a0
1 <a.raziapova@mail.ru>
2023-07-27 01:04:12 +0500

    [add] history in views

--- service_bot/views.py
+++ service_bot/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Message
 
-# Create your views here.
+
+def message_history(request):
+    messages = Message.objects.all()
+    return render(request, 'history.html', {'messages': messages})
