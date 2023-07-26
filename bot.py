2b71e13aeae31df27d63d777ec8c6086c69e80b4
1 <a.raziapova@mail.ru>
2023-07-26 21:42:22 +0500

    [add] weather in telegram bot

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from telegram import Update
 from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = '6297467458:AAFAxPhx1pk5xCGe7EZt7TAmu6UP4frxSxc'
+WEATHER_API_KEY = 'c8a5c9addb456e34d66289ae7c122914'
 NEWS_API_KEY = '542e08dec9a94e3bac7b13452c9f8382'
 START, HELP, WEATHER, NEWS = range(4)
 
@@ -23,8 +24,25 @@ def help_command(update: Update, _: CallbackContext) -> int:
     return START
 
 
-def weather():
-    pass
+def weather(update: Update, _: CallbackContext) -> int:
+    try:
+        city = update.message.text.split(' ', 1)[1]
+    except IndexError:
+        update.message.reply_text("Укажите город после команды /weather, например: /weather Москва")
+        return START
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    if data['cod'] == 200:
+        temperature = data['main']['temp'] - 273.15
+        weather_description = data['weather'][0]['description']
+        update.message.reply_text(f"Текущая погода в городе {city}: {weather_description}, температура: {temperature:.2f}°C")
+    else:
+        update.message.reply_text(f"Не удалось получить данные о погоде в городе {city}. Проверьте правильность названия города.")
+
+    return START
 
 
 def news(update: Update, _: CallbackContext) -> int:
